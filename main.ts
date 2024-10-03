22d470ec364dd1f296070ec007653804acfc38e0
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-03 11:30:53 -0400

    not finished, made code to display color on LED and text on Microbit

--- main.ts
+++ main.ts
@@ -5,4 +5,75 @@
  * This program shows many colours on 1 LED
 */
 
-basic.showString('Hello, World!')
+//setup
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+//turn on RGB LED & display text x7 after A button clicked
+//red
+input.onButtonPressed(Button.A, function() {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//blue
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//green
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//magenta
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//Yellow
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//Cyan
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+//White
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+
+//end
